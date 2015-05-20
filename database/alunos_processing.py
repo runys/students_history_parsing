922766ac4e51454c1b489e6d0b82f4d84b89b6f0
Tiago Pereira <tiagogomespereira@gmail.com>
2015-05-20 07:34:17 -0300

    Added the processing files.

---
+++ database/alunos_processing.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+import json
+from pprint import pprint
+
+# Abrindo o arquivo de alunos
+alunos_file_path = "./json/historicos.json"
+with open(alunos_file_path) as data_file:
+    historicos = json.load(data_file)
+
+# IRA
+def calcularIRA(aluno)
+    ira = 0
+    return ira
+
+for aluno in historicos:
+    
+
+# IRA Programação
+def calcularIRASeletivo(aluno,disciplinas):
+    ira = 0
+    return ira
+
+# Quantas matérias de programação feitas
+def contMaterias(aluno, codigos):
+    return 0
+
+# Fez materia com juiz eletronico?
+def isEjudge(aluno, disiciplinas):
+    return False
+
+# Desempenho antes do juiz
+# Desempenho após o juiz
