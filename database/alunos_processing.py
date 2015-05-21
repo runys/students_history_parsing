2114672226b2d052b72aa94481442ca405ac2faa
Tiago Pereira <tiagogomespereira@gmail.com>
2015-05-21 07:50:57 -0300

    Performance calculation por student.

--- database/alunos_processing.py
+++ database/alunos_processing.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import json
+from sets import Set
 from pprint import pprint
 
 # Abrindo o arquivo de alunos
@@ -9,24 +10,115 @@ with open(alunos_file_path) as data_file:
     historicos = json.load(data_file)
 
 # IRA
-def calcularIRA(aluno)
-    ira = 0
-    return ira
+pesos = {
+    'SR':  0,
+    'II':  1,
+    'MI':  2,
+    'MM':  3,
+    'MS':  4,
+    'SS':  5,
+    'TR': -1,
+    'CC': -1,
+    'AP': -1,
+    'DP': -1,
+    'TJ': -1
+}
 
-for aluno in historicos:
-    
+def calcularIRA(aluno):
+    ira = 0.0
+    num_disciplinas = 0
+    for semestre in aluno["historico"]:
+        for disciplina in semestre["disciplinas"]:
+            if pesos[disciplina["mencao"]] != -1:
+                ira += pesos[disciplina["mencao"]] * int(disciplina["credito"])
+                num_disciplinas += 1
+            
+    return ira / num_disciplinas
+
+#for aluno in historicos:
+#    print("%d: %.2f" % (aluno["id"],calcularIRA(aluno)))
 
 # IRA Programação
+disciplinasProgramacao = Set([
+        "113913",
+        "116301",
+        "195405",
+        "206199",
+        "110141",
+        "195413",
+        "206601",
+        "101095",
+        "208507",
+        "103209",
+        "201286",
+        "206181",
+        "195341",
+        "193704",
+        "203904",
+        "107409",
+        "107417",
+        "206598",
+        "193640",
+        "201294",
+        "208493",
+        "103195",
+        "117552",
+        "203882",
+        "208701"
+    ])
+
 def calcularIRASeletivo(aluno,disciplinas):
-    ira = 0
-    return ira
+    ira = 0.0
+    num_disciplinas = 0
+    for semestre in aluno["historico"]:
+        for disciplina in semestre["disciplinas"]:
+            if pesos[disciplina["mencao"]] != -1 and disciplina["codigo"] in disciplinas:
+                ira += pesos[disciplina["mencao"]] * int(disciplina["credito"])
+                num_disciplinas += 1
+    if num_disciplinas == 0:
+        return 0
+    
+    return ira / num_disciplinas
+
+#for aluno in historicos:
+#    print("%d: %.2f" % (aluno["id"],calcularIRASeletivo(aluno,disciplinasProgramacao)))
+
 
 # Quantas matérias de programação feitas
 def contMaterias(aluno, codigos):
-    return 0
+    cont = 0
+    
+    for semestre in aluno["historico"]:
+        for disciplina in semestre["disciplinas"]:
+            if disciplina["codigo"] in codigos:
+                cont += 1
+    
+    return cont
+
+for aluno in historicos:
+    num_materias = contMaterias(aluno,disciplinasProgramacao)
+    if num_materias > 25:
+        print("%d: %d" % (aluno["id"],num_materias))
+
 
 # Fez materia com juiz eletronico?
-def isEjudge(aluno, disiciplinas):
+disciplinas_ejudge = [
+    {"codigo":"193704","ano":2012,"periodo":2},
+    {"codigo":"193704","ano":2013,"periodo":1},
+    {"codigo":"208493","ano":2012,"periodo":2},
+    {"codigo":"208493","ano":2013,"periodo":1},
+    {"codigo":"208493","ano":2013,"periodo":2},
+    {"codigo":"103195","ano":2013,"periodo":1},
+    {"codigo":"103195","ano":2013,"periodo":2},
+    {"codigo":"103195","ano":2014,"periodo":1},
+    {"codigo":"103195","ano":2014,"periodo":2}
+]
+
+def isEjudge(aluno, disciplinas_ejudge):
+    for semestre in aluno["historico"]:
+        for disciplina in semetre["disciplinas"]:
+            
+    
     return False
 
 # Desempenho antes do juiz
